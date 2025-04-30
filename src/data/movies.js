8c909c445befa882025bf1f06585e87def717d40
judy_u <yuliana.zimirska@lnu.edu.ua>
2025-04-30 22:56:49 +0300

    add movies.js with movie data for display in the application

---
+++ src/data/movies.js
@@ -0,0 +1,110 @@
+export const movies = [
+  {
+    id: 1,
+    title: "Interstellar",
+    description:
+      "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    genre: "Adventure, Drama, Sci-Fi",
+    poster: "/posters/interstellar.jpg",
+    sessionDate: "2025-04-29 20:00",
+  },
+  {
+    id: 2,
+    title: "Titanic",
+    description:
+      "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.",
+    genre: "Drama, Romance",
+    poster: "/posters/titanic.jpg",
+    sessionDate: "2025-05-01 18:00",
+  },
+  {
+    id: 3,
+    title: "Pirates of the Caribbean: At World's End",
+    description:
+      "Captain Jack Sparrow is trapped in Davy Jones' locker, and his friends must rescue him while facing the East India Trading Company.",
+    genre: "Action, Adventure, Fantasy",
+    poster: "/posters/pirates.jpg",
+    sessionDate: "2025-05-03 21:00",
+  },
+  {
+    id: 4,
+    title: "Inception",
+    description:
+      "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.",
+    genre: "Action, Adventure, Sci-Fi",
+    poster: "/posters/inception.jpg",
+    sessionDate: "2025-05-05 19:00",
+  },
+  {
+    id: 5,
+    title: "Pride and Prejudice",
+    description:
+      "Sparks fly when spirited Elizabeth Bennet meets single, rich, and proud Mr. Darcy.",
+    genre: "Drama, Romance",
+    poster: "/posters/pride_and_prejudice.jpg",
+    sessionDate: "2025-05-07 17:00",
+  },
+  {
+    id: 6,
+    title: "Avengers: Endgame",
+    description:
+      "After the devastating events of Avengers: Infinity War, the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe.",
+    genre: "Action, Adventure, Drama",
+    poster: "/posters/avengers_endgame.jpg",
+    sessionDate: "2025-05-09 20:00",
+  },
+  {
+    id: 7,
+    title: "Captain America: Civil War",
+    description:
+      "Political pressure mounts to install a system of accountability when the actions of the Avengers lead to collateral damage. The new status quo deeply divides friends and heroes.",
+    genre: "Action, Adventure, Sci-Fi",
+    poster: "/posters/captain_civil_war.jpg",
+    sessionDate: "2025-05-11 18:00",
+  },
+  {
+    id: 8,
+    title: "How to Train Your Dragon 3",
+    description:
+      "When Hiccup discovers Toothless isn't the only dragon of his kind, he must seek out an elusive dragon rider to protect their home.",
+    genre: "Animation, Action, Adventure",
+    poster: "/posters/howtotrainyourdragon.jpg",
+    sessionDate: "2025-05-13 16:00",
+  },
+  {
+    id: 9,
+    title: "Harry Potter and the Goblet of Fire",
+    description:
+      "Harry finds himself mysteriously selected as an under-aged competitor in a dangerous multi-wizardary school competition.",
+    genre: "Adventure, Family, Fantasy",
+    poster: "/posters/harry_potter.jpg",
+    sessionDate: "2025-05-15 19:00",
+  },
+  {
+    id: 10,
+    title: "Star Wars: Empire Strikes Back",
+    description:
+      "After the Rebels are brutally overpowered by the Empire on the ice planet Hoth, Luke Skywalker begins Jedi training with Yoda.",
+    genre: "Action, Adventure, Fantasy",
+    poster: "/posters/star_wars.jpg",
+    sessionDate: "2025-05-17 21:00",
+  },
+  {
+    id: 11,
+    title: "Dark Knight Rises",
+    description:
+      "Eight years after the Joker's reign of anarchy, the Dark Knight is forced from his exile to save Gotham",
+    genre: "Action, Crime, Drama",
+    poster: "/posters/dark_knight_rises.jpg",
+    sessionDate: "2025-05-19 20:00",
+  },
+  {
+    id: 12,
+    title: "Green Book",
+    description:
+      "A working-class Italian-American bouncer becomes the driver of an African-American classical pianist on a tour of venues through the 1960s American South.",
+    genre: "Biography, Comedy, Drama",
+    poster: "/posters/green_book.jpg",
+    sessionDate: "2025-05-21 18:00",
+  },
+];
