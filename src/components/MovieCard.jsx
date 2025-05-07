41ca53c81300d2a6a8fa286b42b287f4d1949d52
judy_u <yuliana.zimirska@lnu.edu.ua>
2025-05-07 19:46:04 +0300

    change MovieCard component for improved structure and styling

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,15 +1,17 @@
-// src/components/MovieCard.jsx
 import React from "react";
+import { Link } from "react-router-dom";
 import "./MovieCard.css";
-
 const MovieCard = ({ movie }) => {
   return (
     <div className="movie-card">
-      <img src={movie.poster} alt={movie.title} className="movie-poster" />
-      <h2 className="movie-title">{movie.title}</h2>
-      <p className="movie-description">{movie.description}</p>
-      <p className="movie-genre">Genre: {movie.genre}</p>
-      <p className="movie-session">Session: {movie.sessionDate}</p>
+      <img src={movie.poster} alt={movie.title} />
+      <h3>{movie.title}</h3>
+      <p>{movie.description}</p>
+      <p> Genre: {movie.genre}</p>
+      <p> Time: {movie.time}</p>
+      <Link to={`/booking/${movie.id}`}>
+        <button className="book-now-btn">Забронювати</button>
+      </Link>
     </div>
   );
 };
